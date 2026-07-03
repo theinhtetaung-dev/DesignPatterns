163e752907163358924570a00c2910e17e5cb2c0
theinhtetaung-dev <theinhtetaung.developer@gmail.com>
2026-07-04 00:04:25 +0630

    feat: implement Singleton design pattern

---
+++ DesignPatterns.Singleton/ConfigSettingV2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Singleton;
+
+public class ConfigSetting
+{
+    public static ConfigSetting _instance;
+
+    public string DbConnectionString { get; set; }
+
+    private ConfigSetting()
+    {
+        DbConnectionString = "Server=myServer;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+    }
+
+    public static ConfigSetting GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ConfigSetting();
+        }
+        return _instance;
+    }
+}
